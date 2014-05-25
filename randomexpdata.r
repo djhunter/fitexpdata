6d12bcecb1cdef3763bb45011c321eb9ddc28b99
David Hunter <dhunter@westmont.edu>
2014-05-25 10:40:50 -0700

    Fixed model (constant goes in exponent).  Added a trial run.

--- randomexpdata.r
+++ randomexpdata.r
@@ -1,3 +1,8 @@
 time <- seq(0,3,0.03)
-noise <- rnorm(101)*0.2
+noise <- rnorm(101)*0.1
 expdata <- 1.23*exp(-1.34*time)+1.56 + noise
+library("gnm")
+saved.fit <- gnm(expdata ~ 1 + Exp(1+time))
+plot(time,expdata)
+lines(time,as.double(saved.fit$fitted.values))
+
